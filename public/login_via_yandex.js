c770e561508496402e474844a03e31d6e8a90b14
Nikita Ivanov <nikita03.1998@mail.ru>
2024-10-21 10:34:55 +0300

    Правка

--- public/login_via_yandex.js
+++ public/login_via_yandex.js
@@ -12,29 +12,30 @@ if (!yaWpData.error) {
             redirect_uri: location.origin + "/wp-json/login_via_yandex/webhook"
         }
         const tokenPageOrigin = location.origin
-        if (yaWpData.button && document.getElementById(yaWpData?.container_id)) {
+        if (yaWpData.button) {
             if (yaWpData.container_id) {
-                YaAuthSuggest.init(oauthQueryParams, tokenPageOrigin,
-                    {
-                        view: "button",
-                        parentId: yaWpData.container_id,
-                        buttonSize: 'xl',
-                        buttonView: 'main',
-                        buttonTheme: 'light',
-                        buttonBorderRadius: "0",
-                        buttonIcon: 'ya',
-                    }
-                )
-                    .then(({handler}) => handler())
-                    .then(data => console.log('Сообщение с токеном', data))
-                    .catch(error => console.log('Обработка ошибки', error))
-
+                if (document.getElementById(yaWpData.container_id)) {
+                    YaAuthSuggest.init(oauthQueryParams, tokenPageOrigin,
+                        {
+                            view: "button",
+                            parentId: yaWpData.container_id,
+                            buttonSize: 'xl',
+                            buttonView: 'main',
+                            buttonTheme: 'light',
+                            buttonBorderRadius: "0",
+                            buttonIcon: 'ya',
+                        }
+                    )
+                        .then(({handler}) => handler())
+                        .then(data => console.log('Сообщение с токеном', data))
+                        .catch(error => console.log('Обработка ошибки', error))
+                }
             } else {
                 console.log('Не указан ID контейнера для кнопки авторизации через Яндекс ID')
             }
         }
         if (yaWpData.widget) {
-            setTimeout(()=> {
+            setTimeout(() => {
                 YaAuthSuggest.init(oauthQueryParams, tokenPageOrigin)
                     .then(({handler}) => handler())
                     .then(data => console.log('Сообщение с токеном', data))
